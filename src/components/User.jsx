dbed1f25e8a2380bb789464e44599e5441879904
Alexandra2405 <58988130+Alexandra2405@users.noreply.github.com>
2020-01-20 17:18:08 +0300

    Create_user_list

---
+++ src/components/User.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+export const User = props => {
+    return (
+        <div className="user-list__item">
+            <h2>Name: {props.name}</h2>
+            <div>Age: {props.age}</div>
+            <div>Sex: {props.sex}</div>
+        </div>
+    )
+}
+
+export default User
